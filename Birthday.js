59d6c943daebc41dce34585f76ac13c0759b0d4b
Ben <hanrahan.ben@gmail.com>
2024-04-25 08:11:37 -0400

    change name of file

--- Birthday.js
+++ Birthday.js
@@ -1,8 +1,4 @@
 function main() {
-  const birthdayString = "25041998"; // Example birthday input
-  const birthday = new Birthday(birthdayString);
-  const daysUntilNextBirthday = birthday.getDaysUntilNextBirthday();
-  console.log(`Days until your next birthday: ${daysUntilNextBirthday}`);
 }
 
 main();
